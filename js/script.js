7fe7a633df55a13b300f532de82e282a973bc32d
Ylenia <yleniamighela@gmail.com>
2025-01-22 16:15:00 +0100

    added array empty and ciclo for with first step axios

--- js/script.js
+++ js/script.js
@@ -8,17 +8,34 @@ const listaEmail = document.getElementById('emailLista');
 console.log(listaEmail);
 
 
+// creao un array vuoto
+
+const contenitoreEmail = [];
 
 
 // creo un ciclo un for che mi genera 10 indirizzi email
 
+for (let i = 0; i < 10; i++){
+    
+    // per ottenere i dati richiamo la libreria axios,generandomi l'email tramite l'API
+    axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+    
+    .then(risposta => {
+        // da questo consolelog riesco a vedere gli interi oggetti
+        console.log(risposta);
+        
+        
+    })
+    
+    
+    
+    
+};
 
 
 
-// per ottenere dati richiamo la libreria axios,generandomi l'email tramite l'API
 
 // una volta preso il dato dell'email lo "stampo" in console
 
 // successivamente lo stampo in pagina (HTML)
 
-// tutto ciò verrà salvato all'interno di un array
