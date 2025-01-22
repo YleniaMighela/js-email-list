d3b4d4b4ec3c1c236f50bc6decd76ecafc952cee
Ylenia <yleniamighela@gmail.com>
2025-01-22 15:48:19 +0100

    added logic js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 // Attraverso l’apposita API di Boolean
 //  https://flynn.boolean.careers/exercises/api/random/mail
 // generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+
+
+
+
+
+
+// creo un ciclo un for che mi genera 10 indirizzi email
+
+// per ottenere dati richiamo la libreria axios,generandomi l'email tramite l'API
+
+// una volta preso il dato dell'email lo "stampo" in console
+
+// successivamente lo stampo in pagina (HTML)
+
+// tutto ciò verrà salvato all'interno di un array
